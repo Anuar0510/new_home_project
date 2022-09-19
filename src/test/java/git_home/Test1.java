63a8ce047daa869b807200f0e4b898f99eb87e2d
AOmar <omar051000@gmail.com>
2022-09-19 23:40:04 +0600

    fic logic

--- src/test/java/git_home/Test1.java
+++ src/test/java/git_home/Test1.java
@@ -6,10 +6,10 @@ import org.junit.jupiter.api.Test;
 public class Test1 {
     @Test
     void exampleTest1(){
-        Assertions.assertTrue(2>1);
+        Assertions.assertTrue(2>=1);
     }
     @Test
     void exampleTest2(){
-        Assertions.assertTrue(3>1);
+        Assertions.assertTrue(3>=1);
     }
 }
