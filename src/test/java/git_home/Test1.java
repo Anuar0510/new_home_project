9808dd6e04c74f2135e38081b1f9e92564a1d31e
AOmar <omar051000@gmail.com>
2022-09-19 23:43:45 +0600

    use random

--- src/test/java/git_home/Test1.java
+++ src/test/java/git_home/Test1.java
@@ -3,13 +3,15 @@ package git_home;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+
 public class Test1 {
     @Test
     void exampleTest1(){
-        Assertions.assertTrue(2>1);
+        Assertions.assertTrue(new Random().nextInt() >1);
     }
     @Test
     void exampleTest2(){
-        Assertions.assertTrue(3>1);
+        Assertions.assertTrue(new Random().nextInt()>2);
     }
 }
