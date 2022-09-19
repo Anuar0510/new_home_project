14febf49f703fd55d24483f0778d6bcf6792cd32
AOmar <omar051000@gmail.com>
2022-09-19 23:18:26 +0600

    init commit from console

---
+++ src/test/java/git_home/Test1.java
@@ -0,0 +1,11 @@
+package git_home;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Test1 {
+    @Test
+    void exampleTest(){
+        Assertions.assertTrue(2>1);
+    }
+}
